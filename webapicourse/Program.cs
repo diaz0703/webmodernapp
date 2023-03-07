0bb1fb0088eeb21bb2e66e9b9fa3c5aed8f6c218
José Francisco Díaz Castillo <jodiazc@microsoft.com>
2023-03-07 12:01:47 -0600

    van las polizas

--- webapicourse/Program.cs
+++ webapicourse/Program.cs
@@ -17,6 +17,37 @@ builder.Services.AddAuthentication(o =>
     o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 }).AddJwtBearer(o => _config.Bind("AzureAd", o));
 
+//builder.Services.AddAuthorization(o =>
+//{
+//    o.AddPolicy("permisoweather", p =>
+//    {
+//        p.RequireAssertion(c => c.User.HasClaim(o =>
+//                                {
+//                                    bool _result = false;
+//                                    if (o.Type == "http://schemas.microsoft.com/identity/claims/scope")
+//                                    {
+//                                        string[] _valores = o.Value.Split(' ');
+//                                        _result = _valores.Contains("miotropermiso");
+//                                    }
+//                                    return _result;
+//                                }));
+//    });
+//    o.AddPolicy("permisototal", p =>
+//    {
+//        p.RequireAssertion(c => c.User.HasClaim(o =>
+//        {
+//            bool _result = false;
+//            if (o.Type == "http://schemas.microsoft.com/identity/claims/scope")
+//            {
+//                string[] _valores = o.Value.Split(' ');
+//                _result = _valores.Contains("mipermiso");
+//            }
+//            return _result;
+//        }));
+//    });
+//});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
